bb0298f56cb6355053abde830f42fded2f28b146
kpekala <kgpekala@gmail.com>
2022-05-14 15:29:22 +0200

    feat(grid): add displaying a simple grid

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
+import { Button } from '_/common/components';
 import {Grid} from './grid/Grid';
+import './Game.css'
 
 
 export function Game(props: any) {
     return (
         <div className='Game'>
-            <Grid/>
+            <Grid onClick={() => {null;}}/>
         </div>
     );
 }
--- src/components/grid/Grid.tsx
+++ src/components/grid/Grid.tsx
@@ -1,9 +1,49 @@
 import * as React from 'react';
+import './Grid.css';
 
-export function Grid(props: any) {
-    return (
+interface GridProps{
+    onClick: () => void;
+}
+
+export class Grid extends React.Component<GridProps>{
+
+    constructor(props: GridProps){
+        super(props);
+    }
+
+    renderRow(columnsSize: number){
+        const squares = [];
+        for(let i=0; i<columnsSize; i++){
+            squares.push(Square({}));
+        }
+
+        return (<div className="board-row">
+            {squares} 
+        </div>);
+    }
+
+    renderGrid(rowSize: number, columnsSize: number){
+        const rows = [];
+        for(let i=0; i<rowSize; i++){
+            rows.push(this.renderRow(columnsSize));
+        }
+
+        return rows;
+    }
+    render(){
+        return (
         <div className='Grid'>
-
+            {this.renderGrid(10,10)}
         </div>
+        );
+    }
+}
+
+function Square(props: any) {
+    return (
+      <div className="Square"
+       onClick={props.onClick}>
+      </div>
     );
 }
+
