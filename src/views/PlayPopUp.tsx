24a878ffff9a1e4140214be0d8d8b742566e0d28
Filip Tłuszcz <tluszczykin4matyk@gmail.com>
2022-05-15 11:57:16 +0200

    buggy poopups update

---
+++ src/views/PlayPopUp.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { MenuButton } from './MenuButton';
+import { TextInput } from './TextInput';
+import './PlayPopUp.scss'
+
+
+export interface PlayPopUpProps {
+    onClose?: () => void;
+    onCreateNew: () => void;
+    onJoin: (token: string) => void;
+}
+
+export function PlayPopUp(props: PlayPopUpProps) {
+
+    return (
+        <div className='PlayPopUp'>
+            <div className='playPopUpInnerContainer'>
+                <div 
+                    className='escapeButton'
+                    onClick={e => {if(props.onClose)props.onClose();}}
+                ></div>
+                <span>Chcesz stworzyć nowy pokój, czy dołączyć do istniejącego?</span>
+                <div className='buttonContainer'>
+                    <MenuButton onClick={() => props.onCreateNew()}>Nowy</MenuButton>
+                    <TextInput 
+                        placeholder='Dołącz...' 
+                        onSubmit={(token) => props.onJoin(token)}
+                    ></TextInput>
+                </div>
+            </div>
+        </div>
+    );
+}
